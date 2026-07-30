03846d02b1541fcb464531bbd926f5627a09c4d8
Devesh176 <me22b176@smail.iitm.ac.in>
2026-07-30 12:05:39 +0530

    made some changes to debug training

--- custom_GPT/train.py
+++ custom_GPT/train.py
@@ -68,9 +68,10 @@ def generate_sample(model, tokenizer, device, start_context):
 def train_model(model, train_loader, val_loader, device, optimizer, num_epochs, eval_iter, tokenizer, start_context, eval_freq):
     
     train_losses, val_losses = [], []
-    
+    print(f"Starting the Epochs {num_epochs}")
     for epoch in range(num_epochs):
         model.train()  # Set the model to training mode
+        print("Training batches from train_loader")
         for batch in train_loader:
             inputs, targets = batch
             inputs, targets = inputs.to(device), targets.to(device)
@@ -79,16 +80,19 @@ def train_model(model, train_loader, val_loader, device, optimizer, num_epochs, 
             loss = torch.nn.functional.cross_entropy(outputs.view(-1, outputs.size(-1)), targets.view(-1))
             loss.backward()  # Backpropagation
             optimizer.step()  # Update parameters
-
-            if (epoch + 1) % eval_freq == 0:
-                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
-                print(f"Epoch [{epoch + 1}/{num_epochs}], Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
-                train_losses.append(train_loss)
-                val_losses.append(val_loss)
-
+        print("finished the batches from train_loader")
+        if (epoch + 1) % eval_freq == 0:
+            print("Evaluating the model")
+            train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+            print(f"Epoch [{epoch + 1}/{num_epochs}], Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+        torch.cuda.synchronize()
+        print("GPU synchronized")
         # Evaluate the model after each epoch
         # train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
         # print(f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+        print("Generating sample")
         generate_sample(model, tokenizer, device, start_context)
     
     return train_losses, val_losses
